c75dd7d4424dc1aa8ce70f94ee181f4129df7633
Do Ngoc Sang <37763568+nsangrb@users.noreply.github.com>
2021-07-17 22:07:34 +0700

    Update Gen_DXL.js

--- Gen_DXL.js
+++ Gen_DXL.js
@@ -1,7 +1,10 @@
+const { Get_OverviewInfo } = require("./Func");
+const { writeFile } = require("fs");
+const { exec } = require("child_process");
 const { readFile, utils } = require("xlsx");
-const packages = require("fs");
-const { writeFile } = packages;
 
+module.exports = { Generate_Dxl };
+//#region Template
 const for_loop_template = `for (loop = 0; loop<%TESTCASES_CNT%; loop++)
 {
     bool IsErrorOccured = false
@@ -93,63 +96,83 @@ if (IsErrorOccured == false)
 close(targetRequirementModule)
 //print "Requested: " NumberTCRequested " test cases -- Updated: " NumberTCUpdated " test cases \n
 `;
+//#endregion
 
-let linkModule = "/Linkmodule/Verifies_DAS_SUZ_02";
-let Req_Module =
-  "System and Software Requirements and Structure_DAS_SUZ_02_YEA - ENG2 ENG3 ENG4 ENG5";
-let TestSpec_Module = "DAS_SUZ_02_YEA_SW_TST_ENG8_Test_Specification";
-
-var wb = readFile("F:\\JS_WorkSpace\\Testcases_gen\\TestSpec_out_after.xls");
-
-var data = utils.sheet_to_json(wb.Sheets["Test spec"], {
-  blankrows: false,
-});
-let Attr_keys = Object.keys(data[0]);
-
-let define_attrs = "";
-let currobj_infos = "";
-let result = "";
-for (var column = 2; column < Attr_keys.length; column++) {
-  define_attrs += `${Attr_keys[column].replace(/ /g, "_")} = "${
-    Attr_keys[column]
-  }"\n`;
-  currobj_infos += `\t\t\tcurrOject.${Attr_keys[column].replace(
-    / /g,
-    "_"
-  )}\t=(string get(ArrObjectInfor0,loop,${column}))\n`;
+function Read_Config(excel_config_path) {
+  let wb = readFile(excel_config_path);
+  return Get_OverviewInfo(
+    {
+      "TestSpec path": "",
+      "Link module": "",
+      "Requirement Module": "",
+      "TestSpec Module": "",
+    },
+    wb.Sheets["Overview"],
+    "B7:C11"
+  );
 }
 
-result += define_attrs;
-result += `RequirementModuleName="${Req_Module}"\n`;
-result += `currMod="${TestSpec_Module}"\n`;
-result += `Module m0 = null\nif (!null currMod){m0 = edit(currMod, true)}\nArray ArrObjectInfor0 = create(${
-  data.length - 1
-},${Attr_keys.length})\n`;
-result += `
-//Calculate Path
-Project project = getParentProject(m0)
-pathToRequirement = "/" name(project) "/20_SYS/" RequirementModuleName
-pathToLinkModule = "/" name(project) "${linkModule}"\n
-`;
-result += "//Array data\n";
-for (var line = 1; line < data.length; line++) {
-  for (var column = 0; column < Attr_keys.length; column++) {
-    //console.log(data[line][Attr_keys[column]]);
-    let corr_str = data[line][Attr_keys[column]].toString();
-    corr_str = corr_str.replace(/≤/g, "<=");
-    corr_str = corr_str.replace(/≥/g, ">=");
-    //corr_str = corr_str.replace(/\r\n/g, "");
-    corr_str = corr_str.replace(/"/g, "'");
-    if (column > 1)
-      result += `put(ArrObjectInfor0, "${corr_str}", ${line - 1}, ${column})\n`;
-    else {
-      result += `put(ArrObjectInfor0, ${corr_str}, ${line - 1}, ${column})\n`;
+function Generate_Dxl(excel_config_path) {
+  let [err, Overview_info] = Read_Config(excel_config_path);
+  if (err !== "") return err;
+  let linkModule = Overview_info["Link module"];
+  let Req_Module = Overview_info["Requirement Module"];
+  let TestSpec_Module = Overview_info["TestSpec Module"];
+  let wb = readFile(Overview_info["TestSpec path"]);
+  var data = utils.sheet_to_json(wb.Sheets["Test spec"], {
+    blankrows: false,
+  });
+  let Attr_keys = Object.keys(data[0]);
+
+  let define_attrs = "";
+  let currobj_infos = "";
+  let result = "";
+  for (var column = 2; column < Attr_keys.length; column++) {
+    define_attrs += `${Attr_keys[column].replace(/ /g, "_")} = "${
+      Attr_keys[column]
+    }"\n`;
+    currobj_infos += `\t\t\tcurrOject.${Attr_keys[column].replace(
+      / /g,
+      "_"
+    )}\t=(string get(ArrObjectInfor0,loop,${column}))\n`;
+  }
+
+  result += define_attrs;
+  result += `RequirementModuleName="${Req_Module}"\n`;
+  result += `currMod="${TestSpec_Module}"\n`;
+  result += `Module m0 = null\nif (!null currMod){m0 = edit(currMod, true)}\nArray ArrObjectInfor0 = create(${
+    data.length - 1
+  },${Attr_keys.length})\n`;
+  result += `
+  //Calculate Path
+  Project project = getParentProject(m0)
+  pathToRequirement = "/" name(project) "/20_SYS/" RequirementModuleName
+  pathToLinkModule = "/" name(project) "${linkModule}"\n
+  `;
+  result += "//Array data\n";
+  for (var line = 1; line < data.length; line++) {
+    for (var column = 0; column < Attr_keys.length; column++) {
+      //console.log(data[line][Attr_keys[column]]);
+      let corr_str = data[line][Attr_keys[column]].toString();
+      corr_str = corr_str.replace(/≤/g, "<=");
+      corr_str = corr_str.replace(/≥/g, ">=");
+      //corr_str = corr_str.replace(/\r\n/g, "");
+      corr_str = corr_str.replace(/"/g, "'");
+      if (column > 1)
+        result += `put(ArrObjectInfor0, "${corr_str}", ${
+          line - 1
+        }, ${column})\n`;
+      else {
+        result += `put(ArrObjectInfor0, ${corr_str}, ${line - 1}, ${column})\n`;
+      }
     }
   }
+  result += for_loop_template
+    .replace(/%TESTCASES_CNT%/g, data.length - 1)
+    .replace(/%CURROBJ_INFOS%/, currobj_infos);
+  writeFile("Result.dxl", result, function (error) {
+    if (error) err += error.stack + "\r\n";
+  });
+  exec(`notepad "Result.dxl"`);
+  return err;
 }
-result += for_loop_template
-  .replace(/%TESTCASES_CNT%/g, data.length - 1)
-  .replace(/%CURROBJ_INFOS%/, currobj_infos);
-writeFile("Result.dxl", result, function (error) {
-  if (error) err += error.stack + "\r\n";
-});
